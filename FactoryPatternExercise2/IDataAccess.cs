9627bce3ed65fae6eaac7a9c6717063ca6277a76
Lisa Walters <lisacampiwalters@gmail.com>
2023-04-17 22:28:04 -0600

    initial commit--created Product class and IDataAccess

---
+++ FactoryPatternExercise2/IDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public interface IDataAccess
+    {
+        public void SaveData();
+        public void LoadData();
+    }
+}
