5da3a9537800d823c260d01465719cc93b540063
Lisa Walters <lisacampiwalters@gmail.com>
2023-04-18 01:07:36 -0600

    added loops and completed project

--- FactoryPatternExercise2/DataAccessFactory.cs
+++ FactoryPatternExercise2/DataAccessFactory.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace FactoryPatternExercise2
 {
-    internal class DataAccessFactory
+    public static class DataAccessFactory
     {
+        public static IDataAccess GetDataAccessType(string databaseType)
+        { 
+            switch (databaseType.ToLower()) 
+            {
+                case "sql":
+                    return new SQLDataAccess();
+                case "mongo":
+                    return new MongoDataAccess();
+                case "list":
+                    return new ListDataAccess();
+                default:
+                    return new MongoDataAccess();
+            }
+        }
     }
 }
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string response;
+            bool answerIsCorrect;
+
+            do
+
+            {
+                Console.Clear();
+                answerIsCorrect = true;
+                Console.WriteLine("Hello! What database would you like to use? Please type list, SQL, or mongo: ");
+                response = Console.ReadLine();
+                string lowerResponse = response.ToLower();
+
+                if (lowerResponse != "sql" && lowerResponse != "list" && lowerResponse != "mongo")
+                { 
+                    Console.WriteLine("I'm sorry, that was not a valid response");
+                    Thread.Sleep(1000);
+                    answerIsCorrect = false;
+                }
+            } while
+            (!answerIsCorrect);
+
+          IDataAccess db = DataAccessFactory.GetDataAccessType(response);
+
+           var products =  db.LoadData();
+            db.SaveData();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Name: {product.Name}, and  Price: {product.Price}");
+            }
         }
     }
 }
+ 
